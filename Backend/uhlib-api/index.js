9ca477fe1ff621522eeae647262944a057a514a4
Minh Pascal <minhpascal@gmail.com>
2021-03-29 18:54:46 -0500

    API with getItems

---
+++ Backend/uhlib-api/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+const itemsRouter = require('./routes/items');
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({'message': 'api ok'});
+})
+
+app.use('/items', itemsRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`http://localhost:${port}`)
+});
