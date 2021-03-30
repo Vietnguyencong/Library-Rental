ff53ff2117582b81f7c763275c25c397f4c479b6
Minh Pascal <minhpascal@gmail.com>
2021-03-29 19:25:24 -0500

    Added dotenv requirements

--- Backend/uhlib-api/config.js
+++ Backend/uhlib-api/config.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const env = process.env;
 
 const config = {
   db: { 
-    host: env.DB_HOST ,
+    host: env.DB_HOST,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
