3c6c6fd405c1c5bb04b0dfa753e5d16e095e90cd
Vietnguyencong <vietnguyencong99@gmail.com>
2021-03-31 16:07:27 -0500

    add the content-range middleware

--- Backend/uhlib-api/index.js
+++ Backend/uhlib-api/index.js
@@ -15,22 +15,26 @@ app.use(
   })
 );
 
+app.use((req,res,next)=>{
+  console.log("middleware opening")
+  res.header("Content-Range","objects 0-10/10" )
+  next()
+})
 app.get('/', (req, res) => {
   res.json({'message': 'api ok'});
 })
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({'message': err.message});
-  return;
-});
 
 app.use("/transactions", transaction_router)
 app.use("/loanitem", loanitem_router)
 app.use('/api/items', itemsRouter);
 app.use('/api/users', usersRouter);
 
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  return;
+});
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`http://localhost:${port}`)
