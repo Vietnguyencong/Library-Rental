8dc17591f80f567e2a9a7ed2b983fb5dfe707753
Minh Pascal <minhpascal@gmail.com>
2021-04-01 13:57:21 -0500

    Create user on frontend

--- Backend/uhlib-api/routes/users.js
+++ Backend/uhlib-api/routes/users.js
@@ -31,7 +31,6 @@ router.get('/allusers', async function(req, res, next) {
   try {
     res.header('Access-Control-Expose-Headers', 'X-Total-Count');
     count = Object.keys( await usersService.get(req.query.page) ).length;
-    console.log("count:", count);
     res.set("X-Total-Count", count);
     res.setHeader('Content-Range', count);
     res.json(await usersService.get(req.query.page));
@@ -42,7 +41,7 @@ router.get('/allusers', async function(req, res, next) {
 });
 
 /* GETUSER BY ID */
-router.get('/find/:id', async function(req, res, next) {
+router.get('/:id', async function(req, res, next) {
     let id  = req.params.id;
     // console.log('id is ${id}');
     try {
