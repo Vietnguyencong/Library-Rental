8e5088eb6c7c65f67b146a627403e1c68436ae6a
Minh Pascal <minhpascal@gmail.com>
2021-04-03 17:04:05 -0500

    Fixed one user

--- Frontend/uhlibadmin/src/dataproviders/userDataprovider.js
+++ Frontend/uhlibadmin/src/dataproviders/userDataprovider.js
@@ -2,20 +2,11 @@ import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 import { responsiveFontSizes } from '@material-ui/core';
 
-<<<<<<< HEAD
-// const apiUrl = 'http://localhost:5000/api';
-const apiUrl = 'https://uhlib.cc/api'
-const httpClient = fetchUtils.fetchJson;
-
-export default {
-    getList:  async (resource, params) => {
-=======
 const apiUrl = 'http://localhost:5000/api';
 const httpClient = fetchUtils.fetchJson;
 
 export default {
-    getList:  (resource, params) => {
->>>>>>> upstream/main
+    getList:  async (resource, params) => {
         const { page, perPage } = params.pagination;
         const { field, order } = params.sort;
         const query = {
@@ -23,7 +14,6 @@ export default {
             range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
             filter: JSON.stringify(params.filter),
         };
-<<<<<<< HEAD
         // We cna change the sort here 
         // console.log(query)
         // params["sort"]["field"] ="first_name"
@@ -42,60 +32,14 @@ export default {
         const response = await fetch (url)
         const json = await response.json()
         return {data: json}
-=======
-        // const url = `${apiUrl}/${resource}?${stringify(query)}`;
-        const url = `${apiUrl}/${resource}/allusers`;
-
-        // const res = await fetch(url)
-        // const json = await res.json()
-  
-        // const data = {
-        //     data: json.data.map(resource => ({ ...resource, id: resource.user_id }) ),
-        //     total:10
-        // return data
-        // 
-        // } 
-
-        return httpClient(url).then(({ headers, json }) => ({
-            data: json,
-            // data: json.map(resource => ({ ...resource, id: resource.user_id }) ),
-            // total: parseInt(headers.get('Content-Range').split('/').pop(), 10),
-            total: 10,
-        }));
-    },
-
-    getOne: async (resource, params) => {
-        let url = `${apiUrl}/${resource}/find/${params.id}`
-        // let token = ""
-        // if (localStorage.getItem("username")){
-        //     token =   JSON.parse(localStorage.getItem("username")).token
-        // }
-        const response = await fetch (url, {
-            method:"GET",
-            // headers :{
-            //     'Authorization': "bearer " + token
-            // }, 
-        })  
-        const json = await response.json()
-        console.log(json.data)
-        return {data: json.data}
-      
->>>>>>> upstream/main
     },
 
     getMany: (resource, params) => {
         const query = {
-<<<<<<< HEAD
             filter: JSON.stringify({ id: params.ids }),
         };
         const url = `${apiUrl}/${resource}/many?${stringify(query)}`;
         return httpClient(url).then(({ json }) => ({ data: json.map(resource => ({ ...resource, id: resource.user_id }) ), }));
-=======
-            filter: JSON.stringify({ user_id: params.ids }),
-        };
-        const url = `${apiUrl}/${resource}?${stringify(query)}`;
-        return httpClient(url).then(({ json }) => ({ data: json }));
->>>>>>> upstream/main
     },
 
     getManyReference: (resource, params) => {
@@ -109,51 +53,23 @@ export default {
                 [params.target]: params.id,
             }),
         };
-<<<<<<< HEAD
         // console.log("query", query)
         // const url = `${apiUrl}/${resource}/${params.id}`;
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
         console.log(url)
         return httpClient(url).then(({ headers, json }) => ({
             data: [json],
-=======
-        const url = `${apiUrl}/${resource}?${stringify(query)}`;
-
-        return httpClient(url).then(({ headers, json }) => ({
-            data: json,
->>>>>>> upstream/main
             total:10
         }));
     },
 
     update: async(resource, params) =>{
         let url = `${apiUrl}/${resource}/${params.id}`
-<<<<<<< HEAD
         console.log(params.data)
         return httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'PUT',
             body: JSON.stringify(params.data),
         }).then(({ json }) => ({ data: json }))
-=======
-        let token = ""
-        if (localStorage.getItem("username")){
-            token =   JSON.parse(localStorage.getItem("username")).token
-        }
-        const response = await fetch (url, {
-            method:"PUT",
-            headers :{
-                'Authorization': "bearer " + token
-            }, 
-            body: JSON.stringify(params.data),
-        })  
-        const json = await response.json()
-        return {data: json}
-
-        // httpClient(`${apiUrl}/${resource}/${params.id}`, {
-        //     method: 'PUT',
-        //     body: JSON.stringify(params.data),
-        // }).then(({ json }) => ({ data: json }))
->>>>>>> upstream/main
     },
 
     updateMany: (resource, params) => {
@@ -167,11 +83,7 @@ export default {
     },
 
     create: (resource, params) =>
-<<<<<<< HEAD
         httpClient(`${apiUrl}/${resource}/createuser`, {
-=======
-        httpClient(`${apiUrl}/${resource}`, {
->>>>>>> upstream/main
             method: 'POST',
             body: JSON.stringify(params.data),
         }).then(({ json }) => ({
