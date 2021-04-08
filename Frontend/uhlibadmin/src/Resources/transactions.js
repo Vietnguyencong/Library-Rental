f49c835c6a016d22ca2a8c768bb16090ee91dbf8
Vietnguyencong <vietnguyencong99@gmail.com>
2021-04-08 15:05:35 -0500

    add custom boolean textfield

--- Frontend/uhlibadmin/src/Resources/transactions.js
+++ Frontend/uhlibadmin/src/Resources/transactions.js
@@ -1,6 +1,20 @@
 import * as React from "react";
 import { List, Datagrid, TextField, EmailField, DateField, NumberField, ReferenceField, ReferenceInput, TextInput, SimpleForm, Edit, DateInput, NumberInput, SelectInput, BooleanField,BooleanInput,DateTimeInput, Create,Filter, Show, SimpleShowLayout, RichTextField } from 'react-admin';
+import TrueIcon from '@material-ui/icons/Done'
+import FalseIcon from '@material-ui/icons/Clear'
 
+// custom boolean field 
+const MyBooleanfield = ({ record={}, source}) =>{
+    if (record[source] === 1 ){
+        return <div>
+            <TrueIcon/> 
+        </div>
+    }
+    else{
+        return <div><FalseIcon/></div>
+    }
+   
+}
 const TransactionFilter = (props) => (
     <Filter {...props}>
         {/* <TextInput label="Search" source="q" alwaysOn /> */}
@@ -13,7 +27,9 @@ export const TransactionList = props => (
     <List filters={<TransactionFilter/>} {...props}>
         <Datagrid rowClick="edit">
             <ReferenceField source="user_id" reference="users"><TextField source="first_name" /></ReferenceField>
-            <NumberField source="is_commit" />
+            <TextField source="transaction_id" ></TextField>
+            {/* <NumberField source="is_commit" /> */}
+            <MyBooleanfield source="is_commit" />
             <DateField source="date_created" />
             <DateField source="updated_at" />
         </Datagrid>
@@ -26,6 +42,7 @@ export const TransactionEdit = props => (
         <SimpleForm>
             <ReferenceInput source="user_id" reference="users"><SelectInput optionText="first_name" /></ReferenceInput>
             <BooleanInput source="is_commit" />
+            <TextField disabled source="transaction_id" ></TextField>
             <DateTimeInput disabled source="date_created" />
             <DateTimeInput disabled source="updated_at" />
         </SimpleForm>
