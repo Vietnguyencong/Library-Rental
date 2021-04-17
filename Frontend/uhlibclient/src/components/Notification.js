45b43083dbe5bddf73e2f2f0fa2051b5b384997a
Vietnguyencong <vietnguyencong99@gmail.com>
2021-04-17 11:29:14 -0500

    notification created

---
+++ Frontend/uhlibclient/src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from './axios'
+
+class Noti extends React.Component {
+    state = {
+        list_noti : [ ]
+    }
+
+    render (){
+        return (
+            <div>this is the call </div>
+        )
+    }
+}
+export default Noti
