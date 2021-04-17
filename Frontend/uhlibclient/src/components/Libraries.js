1f39f500f8a36110232cc6507a906544a634ab16
Vietnguyencong <vietnguyencong99@gmail.com>
2021-04-17 00:53:22 -0500

    home page

---
+++ Frontend/uhlibclient/src/components/Libraries.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from './axios'
+class  Library extends React.Component { 
+    state =  {
+        libraries : []
+    }
+  componentDidMount(){
+    // fetch the data to get the library data and update teh cart 
+    this.getLibraries()
+  }
+  getLibraries = async () =>{
+    const libraries = await axios.get("https://uhlib.cc/api/libraries/all_libraries")
+    // console.log(libraries)   
+    this.setState({libraries:libraries.data})
+  }
+  render () {
+    return <div class="ui unstackable items"> 
+        {this.state.libraries.map((item)=>{
+            return (
+                <div class="item">
+                    <div class="image">image righr here</div>
+                    <div class="content">
+                        <a class="header">{item.name}</a>
+                        <div class="meta">
+                            <span>{item.opening_hours}</span>
+                        </div>
+                        <div class="extra">
+                            {item.location}
+                        </div>
+                    </div>
+                </div>
+            )
+        })}
+        
+    </div>
+    }
+
+}
+
+export default Library;  
