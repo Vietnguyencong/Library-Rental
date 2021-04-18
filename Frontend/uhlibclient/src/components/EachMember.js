c3a4bee9737609166e0ccd50051113999fc3e19b
Vietnguyencong <vietnguyencong99@gmail.com>
2021-04-18 17:48:06 -0500

    add footer to homepage

--- Frontend/uhlibclient/src/components/EachMember.js
+++ Frontend/uhlibclient/src/components/EachMember.js
@@ -3,7 +3,7 @@ import React from "react";
 class Members extends React.Component {
     render(){
         return (
-            <div class="ui three column grid">
+            <div class="ui three column grid" style={{margin:"10px"}}>
                 <div class="column">
                     <div class="ui fluid card">
                     <div class="image">
---
+++ Frontend/uhlibclient/src/components/helper/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render(){
+        return (
+            <h4 class="ui block header" >
+            <div class="sub header">@coppy right 2021</div>
+            </h4>
+        )
+    }
+}
+
+export default Footer
